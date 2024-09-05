cd955d39d6f64a63f5b29273438bf80e11bd2c34
Andrew <andrey.milutin2010@gmail.com>
2024-09-05 07:04:18 +0300

    Добавил HTML код для мини-приложения

---
+++ db.py
@@ -0,0 +1,29 @@
+import asyncpg
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+async def insert(user_id, username, email):
+    conn = None
+    try:
+        conn = await asyncpg.connect(
+            user='postgres',
+            password='13243511',
+            database='postgres',
+            host='localhost',
+            port=5432,
+        )
+
+        query = """
+        INSERT INTO telegram_users(id, username, record, email)
+        VALUES ($1, $2, 0, $3)
+        ON CONFLICT (id) DO NOTHING;
+        """  # Используйте ON CONFLICT для предотвращения ошибок при вставке дубликатов
+
+        await conn.execute(query, user_id, username, email)
+    except Exception as e:
+        logger.error(f"Ошибка при вставке данных: {e}")
+    finally:
+        if conn:
+            await conn.close()
---
+++ run.py
@@ -0,0 +1,32 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, Router, types, F
+from aiogram.filters import CommandStart, Command
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.storage.memory import MemoryStorage
+from config import TOKEN
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types.web_app_info import WebAppInfo
+
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(storage=MemoryStorage())  # Добавляем хранилище состояний
+router = Router()
+
+
+@router.message(CommandStart())
+async def start(message: types.Message):
+    markup = ReplyKeyboardMarkup(keyboard=[
+        [KeyboardButton(text='Открыть приложение', web_app=WebAppInfo(url='https://google.com'))]], resize_keyboard=True)
+    await bot.send_message(text="Привет", reply_markup=markup, chat_id=message.chat.id)
+
+async def main():
+    logger = logging.getLogger('aiogram')
+    logger.setLevel(logging.INFO)
+    dp.include_router(router)
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
